d15572baa88046c1ddf50606b09422c5772a277b
bogdan demchenko <bogdandemchenko@gmail.com>
2023-01-17 20:09:44 +0200

    api server

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/4johndoe/simplebank/api"
+	db "github.com/4johndoe/simplebank/db/sqlc"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("Cannot start server:", err)
+	}
+}
