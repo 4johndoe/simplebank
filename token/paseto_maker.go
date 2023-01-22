7f00d0a323d5bd463c9d359bf4cc43215404e809
bogdan demchenko <bogdandemchenko@gmail.com>
2023-01-22 23:12:36 +0200

    added refresh token, and sessionid to response

--- token/paseto_maker.go
+++ token/paseto_maker.go
@@ -12,13 +12,14 @@ type PasetoMaker struct {
 	symmetricKey []byte
 }
 
-func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, *Payload, error) {
 	payload, err := NewPayload(username, duration)
 	if err != nil {
-		return "", err
+		return "", payload, err
 	}
 
-	return maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+	token, err := maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+	return token, payload, err
 }
 
 func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
