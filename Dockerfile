cdba92cb54f69d414e89d216347982a6ff880eab
bogdan demchenko <bogdandemchenko@gmail.com>
2023-01-27 00:23:00 +0200

    migrate from go code

--- Dockerfile
+++ Dockerfile
@@ -3,16 +3,16 @@ FROM golang:1.19.5-alpine3.17 AS builder
 WORKDIR /app
 COPY . .
 RUN go build -o main main.go
-RUN apk add curl
-RUN curl -L https://github.com/golang-migrate/migrate/releases/download/v4.15.2/migrate.linux-amd64.tar.gz | tar xvz
+#RUN apk add curl
+#RUN curl -L https://github.com/golang-migrate/migrate/releases/download/v4.15.2/migrate.linux-amd64.tar.gz | tar xvz
 
 # Run stage
 FROM alpine:3.17
 WORKDIR /app
 COPY --from=builder /app/main .
-COPY --from=builder /app/migrate ./migrate
+#COPY --from=builder /app/migrate ./migrate
 COPY app.env .
-COPY db/migration ./migration
+COPY db/migration ./db/migration
 COPY start.sh .
 COPY wait-for.sh .
 
--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	"github.com/4johndoe/simplebank/gapi"
 	"github.com/4johndoe/simplebank/pb"
 	"github.com/4johndoe/simplebank/util"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/rakyll/statik/fs"
@@ -31,9 +34,24 @@ func main() {
 		log.Fatal("Cannot connect to db:", err)
 	}
 
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
 	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	m, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create new migrate instance:", err)
+	}
+
+	if err = m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migrate up:", err)
+	}
+
+	log.Println("db migrated successfully")
 }
 
 func runGrpcServer(config util.Config, store db.Store) {
