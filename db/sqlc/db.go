77c71dbdcc14445604c2002272f0b1151510fca7
bogdan demchenko <bogdandemchenko@gmail.com>
2023-01-28 13:55:05 +0200

    Ft/part update (#11)
    * upd query
    
    * upd mocks
    
    * upd

--- db/sqlc/db.go
+++ db/sqlc/db.go
@@ -87,6 +87,9 @@ func Prepare(ctx context.Context, db DBTX) (*Queries, error) {
 	if q.updateTransferStmt, err = db.PrepareContext(ctx, updateTransfer); err != nil {
 		return nil, fmt.Errorf("error preparing query UpdateTransfer: %w", err)
 	}
+	if q.updateUserStmt, err = db.PrepareContext(ctx, updateUser); err != nil {
+		return nil, fmt.Errorf("error preparing query UpdateUser: %w", err)
+	}
 	return &q, nil
 }
 
@@ -197,6 +200,11 @@ func (q *Queries) Close() error {
 			err = fmt.Errorf("error closing updateTransferStmt: %w", cerr)
 		}
 	}
+	if q.updateUserStmt != nil {
+		if cerr := q.updateUserStmt.Close(); cerr != nil {
+			err = fmt.Errorf("error closing updateUserStmt: %w", cerr)
+		}
+	}
 	return err
 }
 
@@ -257,6 +265,7 @@ type Queries struct {
 	updateAccountStmt       *sql.Stmt
 	updateEntryStmt         *sql.Stmt
 	updateTransferStmt      *sql.Stmt
+	updateUserStmt          *sql.Stmt
 }
 
 func (q *Queries) WithTx(tx *sql.Tx) *Queries {
@@ -284,5 +293,6 @@ func (q *Queries) WithTx(tx *sql.Tx) *Queries {
 		updateAccountStmt:       q.updateAccountStmt,
 		updateEntryStmt:         q.updateEntryStmt,
 		updateTransferStmt:      q.updateTransferStmt,
+		updateUserStmt:          q.updateUserStmt,
 	}
 }
--- db/sqlc/user.sql.go
+++ db/sqlc/user.sql.go
@@ -7,17 +7,16 @@ package db
 
 import (
 	"context"
+	"database/sql"
 )
 
 const createUser = `-- name: CreateUser :one
-INSERT INTO users (
-    username,
-    hashed_password,
-    full_name,
-    email
-) VALUES (
-    $1, $2, $3, $4
-) RETURNING username, hashed_password, full_name, email, password_changed_at, created_at
+INSERT INTO users (username,
+                   hashed_password,
+                   full_name,
+                   email)
+VALUES ($1, $2, $3, $4)
+RETURNING username, hashed_password, full_name, email, password_changed_at, created_at
 `
 
 type CreateUserParams struct {
@@ -47,12 +46,49 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 }
 
 const getUser = `-- name: GetUser :one
-SELECT username, hashed_password, full_name, email, password_changed_at, created_at FROM users
-WHERE username = $1 LIMIT 1
+SELECT username, hashed_password, full_name, email, password_changed_at, created_at
+FROM users
+WHERE username = $1
+LIMIT 1
 `
 
 func (q *Queries) GetUser(ctx context.Context, username string) (User, error) {
 	row := q.queryRow(ctx, q.getUserStmt, getUser, username)
+	var i User
+	err := row.Scan(
+		&i.Username,
+		&i.HashedPassword,
+		&i.FullName,
+		&i.Email,
+		&i.PasswordChangedAt,
+		&i.CreatedAt,
+	)
+	return i, err
+}
+
+const updateUser = `-- name: UpdateUser :one
+UPDATE users
+SET hashed_password = COALESCE($1, hashed_password),
+    full_name = COALESCE($2, full_name),
+    email = COALESCE($3, email)
+WHERE username = $4
+RETURNING username, hashed_password, full_name, email, password_changed_at, created_at
+`
+
+type UpdateUserParams struct {
+	HashedPassword sql.NullString `json:"hashed_password"`
+	FullName       sql.NullString `json:"full_name"`
+	Email          sql.NullString `json:"email"`
+	Username       string         `json:"username"`
+}
+
+func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error) {
+	row := q.queryRow(ctx, q.updateUserStmt, updateUser,
+		arg.HashedPassword,
+		arg.FullName,
+		arg.Email,
+		arg.Username,
+	)
 	var i User
 	err := row.Scan(
 		&i.Username,
