2a2633f14cd910fa5085b3168eb1c11ae1b4d439
bogdan demchenko <bogdandemchenko@gmail.com>
2023-01-29 18:30:38 +0200

    added zerolog with interceptor

--- main.go
+++ main.go
@@ -60,7 +60,8 @@ func runGrpcServer(config util.Config, store db.Store) {
 		log.Fatal("cannot create server:", err)
 	}
 
-	grpcServer := grpc.NewServer()
+	grpcLogger := grpc.UnaryInterceptor(gapi.GrpcLogger)
+	grpcServer := grpc.NewServer(grpcLogger)
 	pb.RegisterSimpleBankServer(grpcServer, server)
 	reflection.Register(grpcServer)
 
