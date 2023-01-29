dcfdd94dc7c9ab9e3e4655e815d77e38c821b16c
bogdan demchenko <bogdandemchenko@gmail.com>
2023-01-29 22:48:07 +0200

    created queue task

---
+++ worker/task_send_verify_email.go
@@ -0,0 +1,36 @@
+package worker
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
+)
+
+type PayloadSendVerifyEmail struct {
+	Username string `json:"username"`
+}
+
+const TaskSendVerifyEmail = "task:send_verify_email"
+
+func (distributor *RedisTaskDistributor) DistributeTaskSendVerifyEmail(
+	ctx context.Context,
+	payload *PayloadSendVerifyEmail,
+	opts ...asynq.Option,
+) error {
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("failed to marshal task payload: %w", err)
+	}
+
+	task := asynq.NewTask(TaskSendVerifyEmail, jsonPayload, opts...)
+	info, err := distributor.client.EnqueueContext(ctx, task)
+	if err != nil {
+		return fmt.Errorf("failed to enqueue task: %w", err)
+	}
+
+	log.Info().Str("type", task.Type()).Bytes("payload", task.Payload()).
+		Str("queue", info.Queue).Int("max_retry", info.MaxRetry).Msg("enqueued task")
+	return nil
+}
